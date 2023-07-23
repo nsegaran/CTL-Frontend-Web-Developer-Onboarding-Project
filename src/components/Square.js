bc96cd8da2a9d9f5caf3a66b650e3aea8ea75451
Suzannah Wistreich <116026916+sdwistreich@users.noreply.github.com>
2023-07-23 18:56:35 +0000

    Update Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,6 +3,7 @@ import React from "react";
 // square component
 const Square = ({value, onClick}) => {
   return (
+    // if square is being clicked by user, use symbol class to change opacity
     <button className={value !== " " ? "squares symbol" : "squares"}
     onClick= {onClick}
     > {value} </button>
